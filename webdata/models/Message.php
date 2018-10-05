7a20af82e8da1f3a81d373732d434897b3972e42
Ronny Wang <ronnywang@gmail.com>
2018-10-05 12:17:24 +0800

    update Channel model columns

--- webdata/models/Message.php
+++ webdata/models/Message.php
@@ -5,12 +5,10 @@ class Message extends Pix_Table
     public function init()
     {
         $this->_name = 'message';
-        $this->_primary = 'ts';
+        $this->_primary = array('channel_id', 'ts');
 
-        $this->_columns['ts'] = array('type' => 'varchar', 'size' => 16);
-        $this->_columns['channel'] = array('type' => 'varchar', 'size' => 16);
+        $this->_columns['ts'] = array('type' => 'NUMERIC');
+        $this->_columns['channel_id'] = array('type' => 'varchar', 'size' => 16);
         $this->_columns['data'] = array('type' => 'json');
-
-        $this->addIndex('channel_ts', array('channel', 'ts'));
     }
 }
