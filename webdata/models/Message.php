56edd4c196612a64da40e8867e63afb77aa27ae4
Ronny Wang <ronnywang@gmail.com>
2018-10-06 12:24:40 +0800

    add message replies view

--- webdata/models/Message.php
+++ webdata/models/Message.php
@@ -11,4 +11,54 @@ class Message extends Pix_Table
         $this->_columns['channel_id'] = array('type' => 'varchar', 'size' => 16);
         $this->_columns['data'] = array('type' => 'json');
     }
+
+    public function getUserName($user_id)
+    {
+        if (!$user_id) {
+            return '@null';
+        }
+        if (!$user = User::find($user_id)) {
+            return '@' . $user_id;
+        }
+        $user_data = json_decode($user->data);
+        if ($user_data->profile->display_name) {
+            return $user_data->profile->display_name;
+        }
+        return $user_data->name;
+    }
+
+    public function getHTML($message_data)
+    {
+        $generated_text = '';
+        $text = $message_data->text;
+        while (true) {
+            if (strpos($text, '<') === false) {
+                $text = str_replace('&lt;', '<', $text);
+                $text = str_replace('&gt;', '>', $text);
+                $generated_text .= nl2br(htmlspecialchars($text));
+                break;
+            }
+            list($plain_text, $other) = explode('<', $text, 2);
+            $plain_text = str_replace('&lt;', '<', $plain_text);
+            $plain_text = str_replace('&gt;', '>', $plain_text);
+            $generated_text .= nl2br(htmlspecialchars($plain_text));
+
+            list($special_text, $text) = explode('>', $other, 2);
+            if ($special_text[0] == '@') {
+                if (!$user = User::find(ltrim($special_text, '@'))) {
+                    $name = '@' . $matches[1];
+                } else {
+                    $name = '@' . $user->name;
+                }
+                $generated_text .= '<b>' . nl2br(htmlspecialchars($name)) . '</b>';
+            } elseif (strpos($special_text, 'http') === 0) {
+                $generated_text .= sprintf("<a href=\"%s\">%s</a>", htmlspecialchars($special_text), htmlspecialchars($special_text));
+            } elseif (preg_match("/^#(.*)\|(.*)$/", $special_text, $matches)) {
+                $generated_text .= sprintf("<a href=\"/index/channel/%s\">#%s</a>", htmlspecialchars($matches[1]), htmlspecialchars($matches[2]));
+            } else {
+                $generated_text .= '&lt;' . $special_text . '&gt;';
+            }
+        }
+        return $generated_text;
+    }
 }
