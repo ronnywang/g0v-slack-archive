1430165001df152b3cb295bae5fce5f6722c20f7
Ronny Wang <ronnywang@gmail.com>
2020-04-28 16:48:13 +0800

    cache fetchMessages error

--- webdata/scripts/update-all.php
+++ webdata/scripts/update-all.php
@@ -6,5 +6,8 @@ Pix_Table::enableLog(Pix_Table::LOG_QUERY);
 User::updateUserData();
 Channel::updateChannelData();
 foreach (Channel::search(1) as $channel) {
-    $channel->fetchMessages();
+    try {
+        $channel->fetchMessages();
+    } catch (Exception $e) {
+    }
 }
